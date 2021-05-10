139690c37474fc80032c090ea3daa1430deab8e9
ArthurZheng <869705086@qq.com>
2021-05-10 14:02:03 +0800

    optimize the script

--- script/download.js
+++ script/download.js
@@ -4,7 +4,7 @@ const rimraf = require('rimraf');
 const path = require('path');
 const fs = require('fs');
 
-const configPath = path.join(__dirname, '../external-resources/config.json');
+const configPath = path.resolve('./external-resources/config.json');
 
 const config = {};
 config.user = 'openblockcc';
@@ -14,24 +14,23 @@ releaseDownloader.getReleaseList(`${config.user}/${config.repo}`)
     .then(release => {
         const latestVersion = release[0].tag_name;
 
-        rimraf(path.join(__dirname, '../external-resources'), () => {
+        rimraf.sync(path.resolve('./external-resources'));
 
-            ghdownload({user: config.user, repo: config.repo, ref: latestVersion},
-                path.join(__dirname, '../external-resources'))
-                .on('error', err => {
-                    console.error(`error while downloading ${config.user}/${config.repo} ${latestVersion}:`, err);
-                })
-                .on('zip', zipUrl => {
-                    console.log(`${zipUrl} downloading...`);
-                })
-                .on('end', () => {
-                    console.log('finish');
+        ghdownload({user: config.user, repo: config.repo, ref: latestVersion},
+            path.resolve('./external-resources'))
+            .on('error', err => {
+                console.error(`error while downloading ${config.user}/${config.repo} ${latestVersion}:`, err);
+            })
+            .on('zip', zipUrl => {
+                console.log(`${zipUrl} downloading...`);
+            })
+            .on('end', () => {
+                console.log('finish');
 
-                    const resourceConfig = require(configPath); // eslint-disable-line global-require
-                    resourceConfig.version = latestVersion;
-                    fs.writeFileSync(configPath, JSON.stringify(resourceConfig));
-                });
-        });
+                const resourceConfig = require(configPath); // eslint-disable-line global-require
+                resourceConfig.version = latestVersion;
+                fs.writeFileSync(configPath, JSON.stringify(resourceConfig));
+            });
     })
     .catch(err => {
         console.log(err);
