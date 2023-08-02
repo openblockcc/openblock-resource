e57f3bc70d293505553d634da9fcfd37f5084d1d
ArthurZheng <869705086@qq.com>
2023-08-02 23:10:58 +0800

    1. Add support for device firmware and arduinoData attributes. 2. Change to read external resources under a single-level directory structure.

--- src/device.js
+++ src/device.js
@@ -37,60 +37,64 @@ class OpenBlockDevice {
 
         parseDeviceList.forEach(deviceId => {
             let matched = false;
-            Object.entries(devices).forEach(catlog => {
-                Object.entries(catlog[1]).forEach(dev => {
-                    const content = dev[1]['index.js'](formatMessage);
+            Object.entries(devices).forEach(dev => {
+                const content = dev[1]['index.js'](formatMessage);
 
-                    const processDeviceData = deviceData => {
-                        if (deviceData.deviceId === deviceId) {
+                const processDeviceData = deviceData => {
+                    if (deviceData.deviceId === deviceId) {
 
-                            const basePath = path.join(this.type, catlog[0], dev[0]);
+                        const basePath = path.join(this.type, dev[0]);
 
-                            // Convert a local file path to a network address
-                            if (deviceData.iconURL) {
-                                deviceData.iconURL = path.join(basePath, deviceData.iconURL);
-                            }
-                            if (deviceData.connectionIconURL) {
-                                deviceData.connectionIconURL = path.join(basePath, deviceData.connectionIconURL);
-                            }
-                            if (deviceData.connectionSmallIconURL) {
-                                deviceData.connectionSmallIconURL = path.join(basePath,
-                                    deviceData.connectionSmallIconURL);
-                            }
-                            if (deviceData.register) {
-                                deviceData.register = path.join(basePath, deviceData.register);
-                            }
-                            if (deviceData.translations) {
-                                deviceData.translations = path.join(basePath, deviceData.translations);
-                            }
-
-                            // filter data based on the edition accessed
-                            if (edition === 'cmtye') {
-                                // when accessing data in the community version, the multi-programming
-                                // framework device only reads the arduino content
-                                if (deviceData.typeList > 1 && deviceData.deviceId.indexOf('arduino') === -1) {
-                                    return;
-                                }
-                                // if the device is not inherited from build-in board, filter it out
-                                if (deviceData.deviceId.indexOf('_') === -1) {
-                                    return;
-                                }
-                            }
-
-                            matched = true;
-                            devicesThumbnailData.push(deviceData);
+                        // Convert a local file path to a network address
+                        if (deviceData.iconURL) {
+                            deviceData.iconURL = path.join(basePath, deviceData.iconURL);
                         }
-                    };
+                        if (deviceData.connectionIconURL) {
+                            deviceData.connectionIconURL = path.join(basePath, deviceData.connectionIconURL);
+                        }
+                        if (deviceData.connectionSmallIconURL) {
+                            deviceData.connectionSmallIconURL = path.join(basePath,
+                                deviceData.connectionSmallIconURL);
+                        }
+                        if (deviceData.register) {
+                            deviceData.register = path.join(basePath, deviceData.register);
+                        }
+                        if (deviceData.translations) {
+                            deviceData.translations = path.join(basePath, deviceData.translations);
+                        }
+                        if (deviceData.firmware) {
+                            deviceData.firmware = path.join(basePath, deviceData.firmware);
+                        }
+                        if (deviceData.arduinoData) {
+                            deviceData.arduinoData = path.join(basePath, deviceData.arduinoData);
+                        }
 
-                    // Support for multiple frameworks device data
-                    if (content.length > 1) {
-                        content.forEach(deviceData => {
-                            processDeviceData(deviceData);
-                        });
-                    } else {
-                        processDeviceData(content);
+                        // filter data based on the edition accessed
+                        if (edition === 'cmtye') {
+                            // when accessing data in the community version, the multi-programming
+                            // framework device only reads the arduino content
+                            if (deviceData.typeList > 1 && deviceData.deviceId.indexOf('arduino') === -1) {
+                                return;
+                            }
+                            // if the device is not inherited from build-in board, filter it out
+                            if (deviceData.deviceId.indexOf('_') === -1) {
+                                return;
+                            }
+                        }
+
+                        matched = true;
+                        devicesThumbnailData.push(deviceData);
                     }
-                });
+                };
+
+                // Support for multiple frameworks device data
+                if (content.length > 1) {
+                    content.forEach(deviceData => {
+                        processDeviceData(deviceData);
+                    });
+                } else {
+                    processDeviceData(content);
+                }
             });
             if (!matched) {
                 devicesThumbnailData.push({deviceId: deviceId});
--- src/extension.js
+++ src/extension.js
@@ -2,8 +2,6 @@ const requireAll = require('require-all');
 const path = require('path');
 const fs = require('fs');
 
-const {EXTENSION_CLASS, DEVICE_TYPE} = require('./config');
-
 const TYPE = 'extensions';
 
 /**
@@ -18,56 +16,52 @@ class OpenBlockExtension {
     assembleData (userDataPath, edition, formatMessage) {
         const extensionsThumbnailData = [];
 
-        DEVICE_TYPE.forEach(deviceType => {
-            EXTENSION_CLASS.forEach(extClass => {
-                const extPath = path.join(userDataPath, this.type, deviceType, extClass);
-                if (fs.existsSync(extPath)) {
-                    const data = requireAll({dirname: extPath, filter: /index.js$/, recursive: true});
-                    Object.entries(data).forEach(ext => {
-                        // Modify the attribute to point to the real address.
-                        const content = ext[1]['index.js'](formatMessage);
-                        const basePath = path.join(this.type, deviceType, extClass, ext[0]);
+        const extPath = path.join(userDataPath, this.type);
+        if (fs.existsSync(extPath)) {
+            const data = requireAll({dirname: extPath, filter: /index.js$/, recursive: true});
+            Object.entries(data).forEach(ext => {
+                // Modify the attribute to point to the real address.
+                const content = ext[1]['index.js'](formatMessage);
+                const basePath = path.join(this.type, ext[0]);
 
-                        // Convert a local file path to a network address
-                        if (content.iconURL) {
-                            content.iconURL = path.join(basePath, content.iconURL);
-                        }
-                        if (content.blocks) {
-                            content.blocks = path.join(basePath, content.blocks);
-                        }
-                        if (content.generator) {
-                            content.generator = path.join(basePath, content.generator);
-                        }
-                        if (content.toolbox) {
-                            content.toolbox = path.join(basePath, content.toolbox);
-                        }
-                        if (content.msg) {
-                            content.msg = path.join(basePath, content.msg);
-                        }
-                        if (content.library) {
-                            content.library = path.join(extPath, ext[0], content.library);
-                        }
-                        if (content.main) {
-                            content.main = path.join(basePath, content.main);
-                        }
-                        if (content.translations) {
-                            content.translations = path.join(basePath, content.translations);
-                        }
-
-                        // filter data based on the edition accessed
-                        if (edition === 'cmtye') {
-                            // if the extension only has main.js but no blocks.js,
-                            // the plugin should be blocked
-                            if (!!content.main && !content.blocks) {
-                                return;
-                            }
-                        }
-
-                        extensionsThumbnailData.push(content);
-                    });
+                // Convert a local file path to a network address
+                if (content.iconURL) {
+                    content.iconURL = path.join(basePath, content.iconURL);
                 }
+                if (content.blocks) {
+                    content.blocks = path.join(basePath, content.blocks);
+                }
+                if (content.generator) {
+                    content.generator = path.join(basePath, content.generator);
+                }
+                if (content.toolbox) {
+                    content.toolbox = path.join(basePath, content.toolbox);
+                }
+                if (content.msg) {
+                    content.msg = path.join(basePath, content.msg);
+                }
+                if (content.library) {
+                    content.library = path.join(userDataPath, basePath, content.library);
+                }
+                if (content.main) {
+                    content.main = path.join(basePath, content.main);
+                }
+                if (content.translations) {
+                    content.translations = path.join(basePath, content.translations);
+                }
+
+                // filter data based on the edition accessed
+                if (edition === 'cmtye') {
+                    // if the extension only has main.js but no blocks.js,
+                    // the plugin should be blocked
+                    if (!!content.main && !content.blocks) {
+                        return;
+                    }
+                }
+
+                extensionsThumbnailData.push(content);
             });
-        });
+        }
 
         return extensionsThumbnailData;
     }
